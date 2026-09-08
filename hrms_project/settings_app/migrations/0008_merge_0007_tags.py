1d12de7e859068171771737ba69fac97d10f7ac1
rezakianimajd <reza.kiani.majd@gmail.com>
2026-09-08 08:08:22 +0330

    Merge conflicting settings_app migrations 0007

---
+++ hrms_project/settings_app/migrations/0008_merge_0007_tags.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('settings_app', '0007_alter_balecontact_tag'),
+        ('settings_app', '0007_signatory_storagePaths'),
+    ]
+
+    operations = [
+    ]
